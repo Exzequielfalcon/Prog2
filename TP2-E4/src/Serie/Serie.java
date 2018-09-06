0322a87b328734e428fdf7d003070290a8cc5a4f
exze <exzequielfalcon@gmail.com>
2018-09-06 15:11:26 -0300

    ej4

---
+++ TP2-E4/src/Serie/Serie.java
@@ -0,0 +1,10 @@
+package Serie;
+
+public class Serie {
+	private String titulo;
+	private String descripcion;
+	private String creador;
+	private String genero;
+	private int temporada[];
+	private Episodio episodios[];
+}
