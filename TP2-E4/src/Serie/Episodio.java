e74ebae8328ecfd76b742c5255369d8dbfc43f5c
exze <exzequielfalcon@gmail.com>
2018-09-08 15:48:47 -0300

    ej4 update

--- TP2-E4/src/Serie/Episodio.java
+++ TP2-E4/src/Serie/Episodio.java
@@ -1,5 +1,73 @@
 package Serie;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Episodio {
-
+	private String titulo;
+	private String descripcion;
+	private boolean flag;
+	private int clasificacion;
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public boolean isFlag() {
+		return flag;
+	}
+	public void setFlag(boolean flag) {
+		this.flag = flag;
+	}
+	public int getClasificacion() {
+		return clasificacion;
+	}
+	public void setClasificacion(int clasificacion) {
+		this.clasificacion = clasificacion;
+	}
+	
+	public Episodio(String titulo, String descripcion, boolean flag) {
+		this.titulo = titulo;
+		this.descripcion = descripcion;
+		this.flag = flag;
+	}
+	public Episodio(String titulo, String descripcion, boolean flag, int clasificacion) {
+		this.titulo = titulo;
+		this.descripcion = descripcion;
+		this.flag = flag;
+		this.clasificacion = clasificacion;
+		CheckClasf();
+	}
+	
+	private void CheckClasf() {
+		if (this.flag==false) {
+			this.clasificacion=-1;
+		}
+	}
+	
+	public void Calificar() {
+		BufferedReader entrada = new BufferedReader(new InputStreamReader(
+				System.in));
+		try {
+			System.out.println("Ingrese una clasificacion entre 1 y 5");
+			int a=new Integer(entrada.readLine());
+			while (a<1||a>5) {
+				System.out.println("Ingrese una clasificacion entre 1 y 5");
+			}
+			this.clasificacion=a;
+		}
+		catch(Exception exc) {
+			System.out.println(exc);
+		}
+	}
+	
+	
+	
 }
--- TP2-E4/src/Serie/Serie.java
+++ TP2-E4/src/Serie/Serie.java
@@ -5,6 +5,19 @@ public class Serie {
 	private String descripcion;
 	private String creador;
 	private String genero;
-	private int temporada[];
 	private Episodio episodios[];
+	private int temporada[];
+	
+	
+	public Serie(String titulo, String descripcion, String creador, String genero, int[] temporada,
+			Episodio[] episodios) {
+		this.titulo = titulo;
+		this.descripcion = descripcion;
+		this.creador = creador;
+		this.genero = genero;
+		this.temporada = temporada;
+		this.episodios = episodios;
+	}
+	
+	
 }
